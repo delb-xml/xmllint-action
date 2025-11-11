62ffe8c78b9bb25c2974bbb875e051fbc303ac5a
Frank Sachsenheim <funkyfuture@risup.net>
2025-11-11 15:18:09 +0100

    First solution attempt

---
+++ action.py
@@ -0,0 +1,137 @@
+import json
+import re
+import subprocess
+from collections.abc import Iterator
+from itertools import batched
+from typing import NamedTuple
+
+from pathlib import Path
+
+from github_custom_actions import ActionBase, ActionInputs, ActionOutputs
+
+
+match_error_message = re.compile(
+    r"(?P<file>.+):(?P<position>\d+): parser error : (?P<message>.+)"
+).fullmatch
+
+
+class Error(NamedTuple):
+    file: Path
+    position: int
+    message: str
+    snippet: str
+
+
+class Inputs(ActionInputs):
+    root_folder: Path = Path(".")
+    file_pattern: str = "**/*.xml"
+    huge_files: bool = False
+
+
+class Outputs(ActionOutputs):
+    errors_html: str
+    errors_json: bytes
+
+
+class Action(ActionBase):
+    inputs: Inputs
+    outputs: Outputs
+
+    def main(self):
+        # newer versions of xmllint also have --pedantic and --strict-namespace
+        self.xmllint_options = ["--noout", "--valid"] + [
+            "--huge"
+        ] * self.inputs.huge_files
+
+        errors: list[Error] = []
+
+        for file in self.iterate_files():
+            errors.extend(self.validate_file(file))
+
+        self.outputs.errors_json = json.dumps([self.error_as_dict(e) for e in errors])
+        self.outputs.errors_html = self.render(
+            """\
+        <table>
+          <thead>
+            <tr>
+            <th>File path</th>
+            <th>Position</th>
+            <th>Error message</th>
+            <th>Snippet</th>
+            </tr>
+          </thead>
+          <tbody>
+            {% for error in errors -%}
+            <tr>
+            <td>{{ error.file }}</td>
+            <td>{{ error.position }}</td>
+            <td>{{ error.message }}</td>
+            <td><pre>{{ error.snippet }}</pre></td>
+            </tr>
+            {%- endfor %}
+          </tbody>
+        </table>
+        """,
+            errors=errors,
+        )
+
+        summary_header = "## xmllint validation report\n\n"
+        if errors:
+            self.summary.text = summary_header + self.outputs.errors_html
+            raise SystemExit(1)
+        else:
+            self.summary.text = summary_header + "Validation succeeded without errors."
+
+    def iterate_files(self) -> Iterator[Path]:
+        for file in (Path.cwd() / self.inputs.root_folder).rglob(
+            self.inputs.file_pattern
+        ):
+            assert file.is_file(follow_symlinks=True)
+            yield file
+
+    def validate_file(self, file: Path) -> list[Error]:
+        process_result = subprocess.run(
+            ["xmlllint"] + self.xmllint_options + [file], capture_output=True, text=True
+        )
+        if errors := self.parse_xmllint_output(process_result.stderr):
+            assert process_result.returncode != 0
+        else:
+            assert process_result.returncode > 0
+        return errors
+
+    def parse_xmllint_output(self, output: str) -> list[Error]:
+        errors: list[Error] = []
+
+        lines = output.splitlines()
+        assert len(lines) % 3 == 0
+
+        for message, snippet, pointer in batched(lines, n=3):
+            assert ": parser error :" in message
+
+            match = match_error_message(message)
+            assert match
+
+            errors.append(
+                Error(
+                    file=match.group("file"),
+                    position=int(match.group("position")),
+                    message=match.group("message"),
+                    snippet=f"{snippet}\n{pointer}",
+                )
+            )
+
+        return errors
+
+    @staticmethod
+    def error_as_dict(error: Error) -> dict:
+        return {
+            "file": error.file,
+            "positon": error.position,
+            "message": error.message,
+            "snippet": error.snippet,
+        }
+
+
+if __name__ == "__main__":
+    # TODO validate xmllint version
+    Action().run()
